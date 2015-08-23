7d3d8c5d7e2ab07eb13d53bf2e0a32821b6f62f1
Payton Swick <payton@foolord.com>
2015-08-23 13:40:17 -0400

    Only save relevant post properties

--- download.js
+++ download.js
@@ -40,7 +40,14 @@ function createPostsDirectory() {
 }
 
 function postToString( post ) {
-	return JSON.stringify( post );
+	var propertiesToCopy = [ 'title', 'likes_enabled', 'discussion', 'page_template', 'date', 'status', 'type', 'post_thumbnail', 'sticky', 'slug', 'sharing_enabled', 'featured_image', 'content' ];
+	var postToSave = propertiesToCopy.reduce( function( prev, key ) {
+		if ( typeof post[ key ] !== 'undefined' ) {
+			prev[ key ] = post[ key ];
+		}
+		return prev;
+	}, {} );
+	return JSON.stringify( postToSave );
 }
 
 module.exports = downloadSite;
