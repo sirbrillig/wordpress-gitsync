4142464c9f624c2ede0a477c1c824e210e463eee
Payton Swick <payton@foolord.com>
2015-09-02 17:00:58 -0400

    Add css download

--- download.js
+++ download.js
@@ -1,4 +1,7 @@
+var wpcom = require( 'wpcom' )();
+
 var fs = require( 'fs' ),
+	debug = require( 'debug' )( 'wpgs:download' ),
 	q = require( 'q' );
 
 var postToString = require( './post-encoding' ).postToString;
@@ -7,8 +10,31 @@ var siteUrl = 'unknown_site';
 
 function downloadSite( site ) {
 	siteUrl = site._id;
+	debug( 'downloadSite', siteUrl );
 	createSiteDirectory().then( function() {
 		downloadPages( site );
+		downloadCss();
+	} );
+}
+
+function downloadCss() {
+	debug( 'downloadCss', siteUrl );
+	q.ninvoke( wpcom.req, 'get', '/sites/' + siteUrl + '/customcss' )
+	.fail( function( err ) {
+		console.error( err );
+	} )
+	.then( function( data ) {
+		var cssType = 'css';
+		if ( ! data.css ) {
+			return;
+		}
+		if ( data.preprocessor === 'sass' ) {
+			cssType = 'scss';
+		}
+		if ( data.preprocessor === 'less' ) {
+			cssType = 'less';
+		}
+		writeCssToFile( data.css, cssType );
 	} );
 }
 
@@ -37,6 +63,15 @@ function writePostToFile( post ) {
 	fs.writeFile( filename, postToString( post ) );
 }
 
+function writeCssToFile( css, type ) {
+	if ( ! type ) {
+		type = 'css';
+	}
+	var filename = getSiteDirectory() + '/' + 'customcss.' + type;
+	console.log( 'writing', filename );
+	fs.writeFile( filename, css );
+}
+
 function getSiteDirectory() {
 	return siteUrl;
 }
@@ -46,6 +81,7 @@ function getPostsDirectory() {
 }
 
 function createSiteDirectory() {
+	// TODO: handle case where directory already exists
 	return q.nfcall( fs.mkdir, getSiteDirectory() );
 }
 
