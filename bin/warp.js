722744bad61d86a8a7eeed668e93a47d87a17169
Payton Swick <payton@foolord.com>
2015-09-11 16:32:39 -0400

    Add missing newline to help text

--- bin/warp.js
+++ bin/warp.js
@@ -13,8 +13,8 @@ var Site = require( '../lib/site' ),
 var helpText = 'wordpress-warp: A tool to download and upload WordPress content and settings to a local directory.\n' +
 	'--site <url>\tOperate on the specified WordPress site. Must be WordPress.com or Jetpack.\n' +
 	'--download\tDownload the site\'s content.\n' +
-	'--upload\tUpload the local version of the site\'s content.' +
-	'--watch\tWhen combined with --upload watches files for changes and upload whenever they change.\n';
+	'--upload\tUpload the local version of the site\'s content.\n' +
+	'--watch\t\tWhen combined with --upload watches files for changes and upload whenever they change.\n';
 
 // Parse command-line arguments
 var argv = parseArgs( process.argv.slice( 2 ), {
